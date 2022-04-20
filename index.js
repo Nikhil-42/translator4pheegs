fda8c36752dcb94bb9de92b3329c9720fd82790d
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2022-04-20 00:01:19 -0400

    revert to bg only

--- index.js
+++ index.js
@@ -144,13 +144,6 @@ convert = function (opt) {
         (keywords[word] || function () { })(); // If one of the words is a `keyword` call its function
     });
 
-    if (Date.now() > 1650427200000) {
-        plaintext_area.value = "Happy Birthday!!!";
-        ciphertext_area.value = "TtaAtt TatTttActAttTacAgg AgtTttAagAcaAgg <ATG";
-        make_bg("https://media.istockphoto.com/vectors/realistic-3d-glossy-balloons-vector-id669645470?k=20&m=669645470&s=612x612&w=0&h=EmlHqnZT87rQj32R3OEJ619EoD_e5FT8jQbBjPhzOhg=")();
-        alert("ACAAatTaaTaaTct AACActTacTtaAcaAtaAatTctcccccccccccccccccccccccccccccc");
-    }
-
     // Update the last_---text values
     last_ciphertext = ciphertext_area.value;
     last_plaintext = plaintext_area.value;
@@ -166,3 +159,5 @@ wipe = function (id) {
 copy = function (id) {
     navigator.clipboard.writeText(document.getElementById(id).value);
 }
+
+make_bg("https://media.istockphoto.com/vectors/realistic-3d-glossy-balloons-vector-id669645470?k=20&m=669645470&s=612x612&w=0&h=EmlHqnZT87rQj32R3OEJ619EoD_e5FT8jQbBjPhzOhg=")();
